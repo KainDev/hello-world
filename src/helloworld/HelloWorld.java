4267236e28170f8a709ba508fa2d65a7a6c012e5
kaindev <65093291+kaindev@users.noreply.github.com>
2020-06-03 11:14:34 +0200

    Finished!

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,26 @@
+/*
+ * Not licensed
+ *
+ */
+
+package helloworld;
+
+/**
+ *
+ * @author kaindev
+ * 
+ * Take care to save this class as "HelloWorld.java"
+ * 
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        // Mi first app - Starting around development
+        
+        System.out.println("Hello World!");
+        
+    }
+        
+}
